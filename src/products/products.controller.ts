0bb9891c8e3a7e95e08bf9bc29255f30a8dd806a
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2021-10-29 20:16:17 +0200

    interfaces

---
+++ src/products/products.controller.ts
@@ -0,0 +1,47 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+import { Product } from './product.interface';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  getAll(): Product[] {
+    return this.productsService.getAll();
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number): Product {
+    return this.productsService.getId(id);
+  }
+
+  @Post()
+  create(@Body() body): Product {
+    return this.productsService.insert(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body): Product {
+    return this.productsService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: number): string {
+    this.productsService.delete(id);
+    return `Borrado`;
+  }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './product.interface';
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Vela aromática',
+      description: 'Esta vela lanza ricos olores',
+    },
+    {
+      id: 2,
+      name: 'Marco de fotos pequeño',
+      description: 'Marco ideal para tus fotos 10x15',
+    },
+  ];
+
+  getAll(): Product[] {
+    return this.products;
+  }
+
+  getId(id: number): Product {
+    const product = this.products.find((item) => item.id == id);
+    if (product) {
+      return product;
+    }
+    throw new NotFoundException('No puedo ');
+  }
+
+  insert(body: any): Product {
+    this.products = [
+      ...this.products,
+      {
+        id: this.lastId() + 1,
+        name: body.name,
+        description: body.description,
+      },
+    ];
+
+    return this.getId(this.lastId());
+  }
+
+  update(id: number, body: any): Product {
+    const product: Product = {
+      id,
+      name: body.name,
+      description: body.description,
+    };
+
+    this.products = this.products.map((item) => {
+      return item.id == id ? product : item;
+    });
+
+    return this.getId(id);
+  }
+
+  delete(id: number): void {
+    this.products = this.products.filter((item) => item.id != id);
+  }
+
+  private lastId(): number {
+    return this.products[this.products.length - 1].id;
+  }
+}
