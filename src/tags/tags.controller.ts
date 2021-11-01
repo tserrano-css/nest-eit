928d139177c0e9d4aaa30301c8d58a3d2b413bab
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2021-11-01 12:47:45 +0100

    Tags

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Tag } from './tag.interface';
+import { TagsService } from './tags.service';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagsService: TagsService) {}
+
+  @Get()
+  getAll(): Tag[] {
+    return this.tagsService.getAll();
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number): Tag {
+    return this.tagsService.getId(id);
+  }
+
+  @Post()
+  create(@Body() body): Tag {
+    return this.tagsService.insert(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body): Tag {
+    return this.tagsService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: number): string {
+    this.tagsService.delete(id);
+    return `Borrado`;
+  }
+}
--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Tag } from './tag.interface';
+
 
 @Injectable()
-export class TagsService {}
+export class TagsService {
+  private tags: Tag[] = [
+    {
+      id: 1,
+      name: 'Muebles',
+      slug: 'muebles',
+    },
+    {
+      id: 2,
+      name: 'Accesorios',
+      slug: 'asccesorios',
+    },
+  ];
+
+  getAll(): Tag[] {
+    return this.tags;
+  }
+
+  getId(id: number): Tag {
+    const tag = this.tags.find((item) => item.id == id);
+    if (tag) {
+      return tag;
+    }
+    throw new NotFoundException('No puedo ');
+  }
+
+  insert(body: any): Tag {
+    this.tags = [
+      ...this.tags,
+      {
+        id: this.lastId() + 1,
+        name: body.name,
+        slug: body.slug,
+      },
+    ];
+
+    return this.getId(this.lastId());
+  }
+
+  update(id: number, body: any): Tag {
+    const product: Tag = {
+      id,
+      name: body.name,
+      slug: body.slug,
+    };
+
+    this.tags = this.tags.map((item) => {
+      return item.id == id ? product : item;
+    });
+
+    return this.getId(id);
+  }
+
+  delete(id: number): void {
+    this.tags = this.tags.filter((item) => item.id != id);
+  }
+
+  private lastId(): number {
+    return this.tags[this.tags.length - 1].id;
+  }
+}
