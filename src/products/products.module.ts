624b12a4a8721baa7aecc040636fe045678b8535
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2021-11-30 18:52:48 +0100

    clase 13: Relaciones en TypeORM

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { Review } from './entities/review.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { ReviewController } from './review/review.controller';
+import { ReviewService } from './review/review.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
-  controllers: [ProductsController],
-  providers: [ProductsService],
+  imports: [TypeOrmModule.forFeature([Product, Review])],
+  controllers: [ProductsController, ReviewController],
+  providers: [ProductsService, ReviewService],
 })
 export class ProductsModule {}
---
+++ src/products/review/review.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { ReviewDto } from '../dto/review.dto';
+import { ReviewService } from './review.service';
+
+@Controller('products')
+export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
+  @Post(':id/review')
+  insertReview(@Param('id', ParseIntPipe) id: string, @Body() body: ReviewDto) {
+    return this.reviewService.insertReview(id, body);
+  }
+}
